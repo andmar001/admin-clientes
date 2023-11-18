e58e3fe86682868a20de053f7b690cd96ef80782
tony tolver <andrademaresantonio00@gmail.com>
2023-11-18 10:49:27 -0600

    ADD primer vista

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-
+  import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ullam libero aspernatur ad officia natus debitis, fuga quas id et hic adipisci, eos earum exercitationem. Odio ea recusandae repellat exercitationem.
-</template>
+    <RouterView />
+  </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Inicio from '../views/ClientesView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: 
+      component: Inicio
     }
   ]
 })
