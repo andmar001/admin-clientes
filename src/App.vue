9fd7f1d2e1da5df94be0643831575089d0ce93a2
tony tolver <andrademaresantonio00@gmail.com>
2023-11-18 10:44:40 -0600

    Primer versión

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ullam libero aspernatur ad officia natus debitis, fuga quas id et hic adipisci, eos earum exercitationem. Odio ea recusandae repellat exercitationem.
+</template>
+
