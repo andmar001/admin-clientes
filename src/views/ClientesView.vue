c006438aad658fc87f6d555b59500a45e0067cd9
tony tolver <andrademaresantonio00@gmail.com>
2023-11-18 12:14:33 -0600

    ADD Routing

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+   import { RouterLink } from 'vue-router';
 </script>
 
 <template>
    <div>
+      <div>
+         <RouterLink :to="{ name:'agregar-cliente'}">
+            <button class="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
+               Agregar Cliente
+            </button>
+         </RouterLink>
+      </div>
       <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
    </div>
 </template>
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,18 @@
+<script setup>
+   import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+   <div>
+      <RouterLink :to="{ name:'inicio'}">
+         <button class="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
+            Volver
+         </button>
+      </RouterLink>
+   </div>
+   <div>
+      <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
+   </div>
+</template>
+
